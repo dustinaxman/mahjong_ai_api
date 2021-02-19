7e26c2a12b84a143978d5638dc74735898e3ae37
dustinaxman <daxman@u.rochester.edu>
2021-02-19 17:01:21 -0500

    [WORKING] made changes to fix the None bug and mad it faster

--- mahjong_ai.py
+++ mahjong_ai.py
@@ -1,10 +1,13 @@
 import random
 import copy
 import numpy as np
+from collections import defaultdict
+import time
+
 
 SUITS = ['bamboo', 'dots', 'character']
 NUMBERS = range(1, 10)
-EXTRA_TILES = [(-1, 'fa tsi')]*4 + \
+EXTRA_TILES = [(-10, 'fa tsi')]*4 + \
 			[(-10, 'hong jong')]*4 + \
 			[(-10, 'bi liar')]*4 + \
 			[(-10, 'bei fong')]*4 + \
@@ -12,14 +15,17 @@ EXTRA_TILES = [(-1, 'fa tsi')]*4 + \
 			[(-10, 'dong fong')]*4 + \
 			[(-10, 'xi fong')]*4
 TILES = ([(number, suit) for suit in SUITS for number in NUMBERS] * 4) + EXTRA_TILES
-SPECIAL_FLUSH_SCORE_FACTOR = 0.05
-NORMAL_FLUSH_SCORE_FACTOR = 0.04
-STRAIGHT_SCORE_FACTOR = 0.03
-NORMAL_DOUBLE_SCORE_FACTOR = 0.01
-SPECIAL_DOUBLE_SCORE_FACTOR = 0.015
-MISSING_SUIT_SCORE_PENALTY_FACTOR = 0.06
+SPECIAL_FLUSH_SCORE_FACTOR = 0.1
+NORMAL_FLUSH_SCORE_FACTOR = 0.08
+STRAIGHT_SCORE_FACTOR = 0.07
+NORMAL_DOUBLE_SCORE_FACTOR = 0.03
+SPECIAL_DOUBLE_SCORE_FACTOR = 0.05
+MISSING_SUIT_SCORE_PENALTY_FACTOR = 0.04
+ONE_OR_NINE_SCORE_FACTOR = 0.02
 
 #TODO:
+
+#fix the while loop pong to not let current player re-pong
 
 # test game with random play (randomish)
 # make sample tree finder
@@ -30,6 +36,7 @@ MISSING_SUIT_SCORE_PENALTY_FACTOR = 0.06
 # heuristic based on how many "winning" options there are
 # add learning part
 # construct fake games meant to teach that you need each suit + 9 1 not 8 and 2
+
 
 class Environment:
 	def __init__(self, tiles, num_players=4):
@@ -53,16 +60,17 @@ class Environment:
 class Brain:
 	def __init__(self):
 		self.NUM_PLAYOUTS = 500
-		self.REGRESSION_THRESHOLD = -0.035
+		self.REGRESSION_THRESHOLD = -0.02
 	def run_playouts(self, game, state):
-		if state[2]:
+		if state[2] is not None:
 			hero_player_idx = state[2]
 		else:
 			hero_player_idx = game.env.current_player_idx
 		total_wins = 0
 		for i in range(self.NUM_PLAYOUTS):
+			game.shuffle_game(hero_player_idx)
 			mcts_game = Game(player_list=copy.deepcopy(game.player_list), env=copy.deepcopy(game.env), MCTS=True, state=state)
-			mcts_game.play(num_turns_to_run=16)
+			mcts_game.play(num_turns_to_run=12)
 			total_wins += mcts_game.player_list[hero_player_idx].score
 			#total_wins += 1.0 if mcts_game.player_list[hero_player_idx].score == 1.0 else 0.0
 		percentage_wins = total_wins / float(self.NUM_PLAYOUTS)
@@ -128,15 +136,33 @@ class Brain:
 			# 			heuristic_filtered_options.append(option)
 			# 	else:
 			# 		heuristic_filtered_options.append(option)
+			# if len(heuristic_filtered_options) != 0:
 			# best_option = heuristic_filtered_options[random.randint(0, len(heuristic_filtered_options)-1)]
-			best_option = options[random.randint(0, len(options)-1)]
+			# else:
+			#best_option = options[random.randint(0, len(options)-1)] #TODO replace with self.get_action_eval
+			#best_option = options[random.randint(0, len(options)-1)]
 		else:
-			print('PLAYER:' + str(hero_player_idx))
 			print('********************')
+			print('********************')
+			print('********************')
+			print('PLAYER:' + str(hero_player_idx))
+			print(game.player_list[hero_player_idx].score)
+			print('********************')
+			print(hero_player.open_groups)
+			print('********************')
+			used_tiles = game.env.discarded + game.player_list[hero_player_idx].hand + [tile for player in game.player_list for group in player.open_groups for tile in group[0]]
+			possible_remaining_tiles = copy.deepcopy(TILES)
+			for tile in used_tiles:
+				possible_remaining_tiles.pop(possible_remaining_tiles.index(tile))
+			tile_count = defaultdict(int)
+			for tile in possible_remaining_tiles:
+				tile_count[tile] += 1
+			for tile, count in sorted(tile_count.items(), key=lambda tile_count: [tile_count[0][1], tile_count[0][0]]):
+				print(str(tile) + " : " + str(count))
 			print('############')
+			start = time.process_time()
 			for option in options:
 				game_option = copy.deepcopy(game)
-				game_option.shuffle_game(hero_player_idx)
 				option_card_idx = game_option.player_list[game_option.env.current_player_idx].hand.index(option)
 				state = ('after_discard', game_option.player_list[game_option.env.current_player_idx].remove_from_hand(option_card_idx), None)
 				percentage_wins = self.run_playouts(game_option, state)
@@ -145,14 +171,16 @@ class Brain:
 			best_option = options[best_option_idx]
 			tmp_map = {}
 			for option_tmp, score_avg in zip(options, option_percentage_wins):
-				tmp_map[option_tmp] = "{:.2f}".format(score_avg)
+				tmp_map[option_tmp] = "{:.3f}".format(score_avg)
 			for option_tmp in sorted(hero_player.hand, key=lambda tile: [tile[1], tile[0]]):
 				print(str(option_tmp) + ": " + tmp_map[option_tmp])
 			print('############')
 			print(best_option)
 			print('############')
+			print(time.process_time() - start)
 			print('********************')
 		return best_option
+
 				
 class Player:
 	def __init__(self):
@@ -233,9 +261,6 @@ class Player:
 			special_flush_count_open = len([group for group in self.open_groups if group[0][0][1] not in SUITS])
 			normal_flush_count_open = len([group for group in self.open_groups if group[0][0][1] in SUITS and group[1] == 'flush'])
 			straight_count_open = len([group for group in self.open_groups if group[1] == 'straight'])
-			score += special_flush_count_open * SPECIAL_FLUSH_SCORE_FACTOR
-			score += normal_flush_count_open * NORMAL_FLUSH_SCORE_FACTOR
-			score += straight_count_open * STRAIGHT_SCORE_FACTOR
 			self.open_suit_list = [group[0][0][1] for group in self.open_groups]
 			num_suits_unopened = len([suit for suit in SUITS if suit not in self.open_suit_list])
 			if num_suits_unopened-1 > (4 - len(self.open_groups)):
@@ -249,6 +274,8 @@ class Player:
 				dict_of_values_for_remaining_tile_calculation['at_least_one_flush'] = False
 				dict_of_values_for_remaining_tile_calculation['doubles_list'] = False
 				dict_of_values_for_remaining_tile_calculation['missing_suits'] = []
+				dict_of_values_for_remaining_tile_calculation['leftover_tiles'] = []
+				dict_of_values_for_remaining_tile_calculation['overlapping_tiles_used'] = True
 				score = 0
 				return score, dict_of_values_for_remaining_tile_calculation
 		hand = sorted(hand, key=lambda tile: [tile[1], tile[0]])
@@ -257,72 +284,51 @@ class Player:
 		special_flush_count = 0
 		normal_flush_count = 0
 		straight_count = 0
+
+		tile_to_index_list_map = defaultdict(list)
+		for i, tile in enumerate(hand):
+			tile_to_index_list_map[tile].append(i)
+			if len(tile_to_index_list_map[tile]) == 3:
+				flush_tile_idxs = tile_to_index_list_map[tile]
+				used_tile_idxs.extend(flush_tile_idxs)
+				flush_suit = hand[flush_tile_idxs[0]][1]
+				if flush_suit in SUITS:
+					normal_flush_count += 1
+				else:
+					special_flush_count += 1
+				suits_counted_towards_score.append(flush_suit)
+				tile_to_index_list_map[tile] = []
+
 		no_straight_flag = False
-		no_flush_flag = False
 		last_tile = None
 		straight_tracker = 0
-		flush_tracker = 0
-		used_tile_candidate_list_flush = []
 		used_tile_candidate_list_straight = []
-		#import pdb
-		#pdb.set_trace()
 		for i, tile in enumerate(hand):
-			if last_tile == (tile[0] - 1, tile[1]):
-				used_tile_candidate_list_straight.append(i)
-				flush_tracker = 0
-				#print('a')
-				#pdb.set_trace()
-				if not no_straight_flag:
-					if straight_tracker < 1:
-						straight_tracker += 1
-					else:
-						used_tile_idxs.extend(used_tile_candidate_list_straight)
-						used_tile_candidate_list_straight = []
-						straight_count += 1
-						no_straight_flag = True
-						straight_tracker = 0
-						suits_counted_towards_score.append(tile[1])
-					#print('b1')
-					#pdb.set_trace()
-				else:
-					no_straight_flag = False
-					#print('b2')
-					#pdb.set_trace()
-			elif last_tile == tile:
-				used_tile_candidate_list_flush.extend([i-1, i])
-				if not no_flush_flag:
-					if flush_tracker < 1:
-						flush_tracker += 1
-					else:
-						used_tile_idxs.extend(used_tile_candidate_list_flush)
-						used_tile_candidate_list_flush = []
-						flush_tracker = 0
-						if tile[1] in SUITS:
-							normal_flush_count += 1
-						else:
-							special_flush_count += 1
-						suits_counted_towards_score.append(tile[1])
-						no_flush_flag = True
-						no_straight_flag = True
-						straight_tracker = 0
-				else:
-					no_flush_flag = False
-			else:
-				used_tile_candidate_list_flush = []
-				used_tile_candidate_list_straight = []
-				flush_tracker = 0
-				straight_tracker = 0
-				no_straight_flag = False
-				no_flush_flag = False
-				if flush_tracker == 0:
-					used_tile_candidate_list_flush.append(i)
-				if straight_tracker == 0:
+			if i not in used_tile_idxs:
+				if last_tile == (tile[0] - 1, tile[1]):
 					used_tile_candidate_list_straight.append(i)
-				#print('c')
-				#pdb.set_trace()
-			last_tile = tile
-		flush_tracker = 0
-		no_flush_flag = False
+					if not no_straight_flag:
+						if straight_tracker < 1:
+							straight_tracker += 1
+						else:
+							used_tile_idxs.extend(used_tile_candidate_list_straight)
+							used_tile_candidate_list_straight = []
+							straight_count += 1
+							no_straight_flag = True
+							straight_tracker = 0
+							suits_counted_towards_score.append(tile[1])
+					else:
+						no_straight_flag = False
+				elif tile == last_tile:
+					no_straight_flag = False
+				else:
+					used_tile_candidate_list_straight = []
+					straight_tracker = 0
+					no_straight_flag = False
+					if straight_tracker == 0:
+						used_tile_candidate_list_straight.append(i)
+				last_tile = tile
+
 		special_double_count = 0
 		normal_double_count = 0
 		doubles_list = []
@@ -337,18 +343,9 @@ class Player:
 					used_tile_idxs.extend([i-1, i])
 					doubles_list.append([hand[i-1], hand[i]])
 					suits_counted_towards_score.append(tile[1])
-				else:
-					flush_tracker = 0
 				last_tile = tile
-		score += special_flush_count * SPECIAL_FLUSH_SCORE_FACTOR
-		score += normal_flush_count * NORMAL_FLUSH_SCORE_FACTOR
-		score += straight_count * STRAIGHT_SCORE_FACTOR
-		score += normal_double_count * NORMAL_DOUBLE_SCORE_FACTOR
-		score += special_double_count * SPECIAL_DOUBLE_SCORE_FACTOR
 
 		missing_suits = [suit for suit in SUITS if suit not in suits_counted_towards_score]
-		missing_suits_score_penalty = max(len(suits_counted_towards_score) + len(missing_suits) - 5, 0) * MISSING_SUIT_SCORE_PENALTY_FACTOR
-		score -= missing_suits_score_penalty
 		num_groups = (special_flush_count + normal_flush_count + straight_count + straight_count_open + special_flush_count_open + normal_flush_count_open)
 		four_groups = num_groups == 4
 		num_doubles = normal_double_count + special_double_count
@@ -361,8 +358,18 @@ class Player:
 		one_or_nine_in_hand = any([tile[0] in [1, 9] for tile in all_tiles_for_player])
 		leftover_tiles = [tile for i, tile in enumerate(hand) if i not in used_tile_idxs]
 		overlapping_tiles_used = (len(leftover_tiles) + 3*num_groups + 2*num_doubles) > len(hand) + 3*len(self.open_groups)
-		if one_or_nine_in_hand:
-			score += 0.005
+		
+		score += one_or_nine_in_hand * ONE_OR_NINE_SCORE_FACTOR
+		score += special_flush_count * SPECIAL_FLUSH_SCORE_FACTOR
+		score += normal_flush_count * NORMAL_FLUSH_SCORE_FACTOR
+		score += straight_count * STRAIGHT_SCORE_FACTOR
+		score += normal_double_count * NORMAL_DOUBLE_SCORE_FACTOR
+		score += special_double_count * SPECIAL_DOUBLE_SCORE_FACTOR
+		score += special_flush_count_open * SPECIAL_FLUSH_SCORE_FACTOR
+		score += normal_flush_count_open * NORMAL_FLUSH_SCORE_FACTOR
+		score += straight_count_open * STRAIGHT_SCORE_FACTOR
+		score -= len(missing_suits) * MISSING_SUIT_SCORE_PENALTY_FACTOR
+
 		win = four_groups \
 			and not overlapping_tiles_used \
 			and one_double \
@@ -429,6 +436,12 @@ class Player:
 					if (at_least_one_special_group or one_or_nine_in_hand):
 						self.winning_tiles = [double[0] for double in doubles_list if double[0][1] in missing_suits]
 			elif num_doubles == 1:
+				if len(leftover_tiles) < 2:
+					print(self.hand)
+					print(leftover_tiles)
+					print(self.open_groups)
+					print(num_groups)
+					raise ValueError()
 				if leftover_tiles[0][0] in [2, 8] and leftover_tiles[0] == leftover_tiles[1]:
 					#remaining is double 2 or 8 and another double
 					self.winning_tiles = [leftover_tiles[0], doubles_list[0][0]]
@@ -440,6 +453,7 @@ class Player:
 						print(leftover_tiles)
 						print(self.open_groups)
 						print(num_groups)
+						raise ValueError()
 					if leftover_tiles[0][0] - leftover_tiles[1][0] >= -2 and leftover_tiles[0][0] - leftover_tiles[1][0] <= 2 and leftover_tiles[0][1] == leftover_tiles[1][1]:
 						candidate_tiles = self.get_missing_of_straight(leftover_tiles)
 						if at_least_one_special_group:
@@ -494,8 +508,10 @@ class Game:
 		random.shuffle(possible_remaining_tiles)
 		for i in range(0,4):
 			if i != hero_player_idx:
-				self.player_list[i].hand = possible_remaining_tiles[(i-1)*13:13*i]
-		self.env.remaining = possible_remaining_tiles[4*13:]
+				num_tiles_to_select = 13 - 3*len(self.player_list[i].open_groups)
+				self.player_list[i].hand = possible_remaining_tiles[0:num_tiles_to_select]
+				del possible_remaining_tiles[:num_tiles_to_select]
+		self.env.remaining = possible_remaining_tiles
 
 	def set_state(self, state):
 		if state[0] == 'do_pong':
@@ -520,12 +536,13 @@ class Game:
 			else:
 				i = state[2] + 1 + self.env.num_players
 			while i < self.env.current_player_idx + self.env.num_players:
-				tiles_opened = self.player_list[i % self.env.num_players].do_pong(self, i % self.env.num_players)
-				if tiles_opened:
-					self.env.current_player_idx = i % self.env.num_players
-					self.env.open_hand(tiles_opened)
-					self.game_end = True
-					return
+				if not i + 1 % self.env.num_players == self.env.current_player_idx:
+					tiles_opened = self.player_list[i % self.env.num_players].do_pong(self, i % self.env.num_players)
+					if tiles_opened:
+						self.env.current_player_idx = i % self.env.num_players
+						self.env.open_hand(tiles_opened)
+						self.game_end = True
+						return
 				i += 1
 			else:
 				tiles_opened = self.player_list[self.env.current_player_idx].do_chi(self)
@@ -584,7 +601,7 @@ class Game:
 		elif state[0] == 'after_discard':
 			discard_tile = state[1]
 			self.env.discard(discard_tile)
-			self.player_list[self.env.current_player_idx].update_winning_tiles()		
+			self.player_list[self.env.current_player_idx].update_winning_tiles()
 			for i in range(self.env.current_player_idx, self.env.current_player_idx + self.env.num_players):
 				if self.env.discarded[-1] in self.player_list[i % self.env.num_players].winning_tiles:
 					self.player_list[i % self.env.num_players].add_to_hand(self.env.discarded.pop(-1))
@@ -598,12 +615,14 @@ class Game:
 	def play_turn(self):
 		self.env.current_player_idx = (self.env.current_player_idx + 1) % self.env.num_players
 		i = self.env.current_player_idx
+		assert(all(len(player.hand)+len(player.open_groups)*3 > 12 for player in self.player_list))
 		while i < self.env.current_player_idx + self.env.num_players:
-			tiles_opened = self.player_list[i % self.env.num_players].do_pong(self, i % self.env.num_players)
-			if tiles_opened:
-				self.env.current_player_idx = i % self.env.num_players
-				self.env.open_hand(tiles_opened)
-				break
+			if not i + 1 % self.env.num_players == self.env.current_player_idx:
+				tiles_opened = self.player_list[i % self.env.num_players].do_pong(self, i % self.env.num_players)
+				if tiles_opened:
+					self.env.current_player_idx = i % self.env.num_players
+					self.env.open_hand(tiles_opened)
+					break
 			i += 1
 		else:
 			tiles_opened = self.player_list[self.env.current_player_idx].do_chi(self)
@@ -615,12 +634,14 @@ class Game:
 					return
 				else:
 					self.current_player_draw()
+		assert(all(len(player.hand)+len(player.open_groups)*3 > 12 for player in self.player_list))
 		self.player_list[self.env.current_player_idx].update_score()
 		if not self.player_list[self.env.current_player_idx].score < 1:
 			self.game_end = True
 			return
 		discard_tile = self.player_list[self.env.current_player_idx].discard(self)
 		self.env.discard(discard_tile)
+		assert(all(len(player.hand)+len(player.open_groups)*3 > 12 for player in self.player_list))
 		self.player_list[self.env.current_player_idx].update_winning_tiles()		
 		for i in range(self.env.current_player_idx, self.env.current_player_idx + self.env.num_players):
 			if self.env.discarded[-1] in self.player_list[i % self.env.num_players].winning_tiles:
